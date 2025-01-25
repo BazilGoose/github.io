22fd4560b390c4c53a19e76c1c6aec5b530d1cc4
BazilGoose <156262039+BazilGoose@users.noreply.github.com>
2025-01-25 14:43:17 -0500

    Added smooth scroll to backToTopButton

--- scripts/app.js
+++ scripts/app.js
@@ -68,7 +68,6 @@
         // Gotten from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
         let backToTopButton = document.getElementById("backToTopButton");
 
-		// When the user scrolls down 20px from the top of the document, show the button
 		window.onscroll = function() {scrollFunction()};
 
 		function scrollFunction() {
@@ -79,10 +78,9 @@
 			}
 		}
 
-		// When the user clicks on the button, scroll to the top of the document
 		backToTopButton.addEventListener("click", function() {
-			document.body.scrollTop = 0; // For Safari
-			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+			// Gotten from: theMaxx and KyleMit at https://stackoverflow.com/questions/15935318/smooth-scroll-to-top
+			window.scrollTo({top: 0, behavior: 'smooth'});
 		});
     }
 

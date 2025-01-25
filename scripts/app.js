14708851dcf73c60fee300ac8eb072f3d3383474
BazilGoose <156262039+BazilGoose@users.noreply.github.com>
2025-01-25 14:38:30 -0500

    Added eventListener to backToTopButton

--- scripts/app.js
+++ scripts/app.js
@@ -80,10 +80,10 @@
 		}
 
 		// When the user clicks on the button, scroll to the top of the document
-		function topFunction() {
+		backToTopButton.addEventListener("click", function() {
 			document.body.scrollTop = 0; // For Safari
 			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-		}
+		});
     }
 
     function DisplayHomePage(){

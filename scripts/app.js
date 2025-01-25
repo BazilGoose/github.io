17f33ff122e3cd0ae23cbb6e73f3904da197c1b6
BazilGoose <156262039+BazilGoose@users.noreply.github.com>
2025-01-25 14:27:55 -0500

    Modifying the backToTopButton

--- scripts/app.js
+++ scripts/app.js
@@ -68,18 +68,22 @@
         // Gotten from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
         let backToTopButton = document.getElementById("backToTopButton");
 
-        window.onscroll = function () {
-            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-                backToTopButton.style.display = "block";
-            } else {
-                backToTopButton.style.display = "none";
-            }
-        };
+		// When the user scrolls down 20px from the top of the document, show the button
+		window.onscroll = function() {scrollFunction()};
 
-        backToTopButton.addEventListener("click", function(){
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        });
+		function scrollFunction() {
+			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+				backToTopButton.style.display = "block";
+			} else {
+				backToTopButton.style.display = "none";
+			}
+		}
+
+		// When the user clicks on the button, scroll to the top of the document
+		function topFunction() {
+			document.body.scrollTop = 0; // For Safari
+			document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+		}
     }
 
     function DisplayHomePage(){
